a11dce64f3389e17d4ce4d80a48d045bc110cf36
unknown <jdavid.stevens@gmail.com>
2019-03-11 14:57:57 -0600

    routing initiated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import {render} from "react-dom";
-import Pet from './Pet';
+import React from "react";
+import {render} from 'react-dom';
+import Results from './Results';
 
-class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
-  render() {
+class App extends React.Component{
+  render(){
     return(
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese"/>
-        <Pet name="Pepper" animal="bird" breed="Cockatiel"/>
-        <Pet name="Doink" animal="cat" breed="Mixed"/>
+        <Results/>
       </div>
     )
   }
 }
-
-render(<App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
 
 //npm run dev runs project in browser on http://localhost:1234
+{/* 
+          dump state into DOM:
+          <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+          </pre> */}
