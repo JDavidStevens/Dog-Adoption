ddc198adf328324546d022bd78d01d902995d3a9
unknown <jdavid.stevens@gmail.com>
2019-03-11 11:59:41 -0600

    lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render } from "react-dom";
+import {render} from "react-dom";
 import Pet from './Pet';
 
 class App extends React.Component {
@@ -7,29 +7,17 @@ class App extends React.Component {
     alert("you clicked the title");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      })
-    ]);
+    return(
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese"/>
+        <Pet name="Pepper" animal="bird" breed="Cockatiel"/>
+        <Pet name="Doink" animal="cat" breed="Mixed"/>
+      </div>
+    )
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
+
+//npm run dev runs project in browser on http://localhost:1234
