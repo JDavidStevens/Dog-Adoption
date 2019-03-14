0c64388e8b0360a8e38c80fabf824fa88cf8633b
unknown <jdavid.stevens@gmail.com>
2019-03-13 21:57:25 -0600

    Carousel

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router/lib/history";
+import Carousel from "./Carousel";
 
-class Details extends React.Component{
-    render(){
-        return <h1>hi lol</h1>
+const petfinder = pf({
+  key: process.env.REACT_APP_API_KEY,
+  secret: process.env.REACT_APP_API_SECRET
+});
+
+class Details extends React.Component {
+  state = {
+    loading: true
+  };
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: "full", id: this.props.id })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city},${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        navigate("/");
+      });
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading ...</h1>;
     }
+    const { name, animal, breed, description, location,media } = this.state;
+    return (
+      <div className="details">
+      <Carousel media={media}/>
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Details;
