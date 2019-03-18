785cfc80c8bb0a92de58422f01d9e48842c91bf8
unknown <jdavid.stevens@gmail.com>
2019-03-18 17:26:38 -0600

    additional search fields

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,33 +1,105 @@
-import React from 'react';
+import React from "react";
+import pf, { ANIMALS } from "petfinder-client";
 
-class SearchParams extends React.Component{
-state={
+const petfinder = pf({
+  key: process.env.REACT_APP_API_KEY,
+  secret: process.env.REACT_APP_API_SECRET
+});
+
+class SearchParams extends React.Component {
+  state = {
     location: "Seattle, WA",
     animal: "",
-    breed: ""
-}
+    breed: "",
+    breeds: []
+  };
 
-//binds to the onChange
-handleLocationChange=event=>{
+  //binds to the onChange
+  handleLocationChange = event => {
     this.setState({
-        location: event.target.value
-    })
-}
-render(){
-    return(
-        <div className="search-params">
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreeds
+    );
+  };
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal }).then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({
+            breeds: data.petfinder.breeds.breed
+          });
+        }
+      });
+    } else {
+      this.setState({ breeds: [] });
+    }
+  }
+  render() {
+    return (
+      <div className="search-params">
         <label htmlFor="location">
-        Location
-        <input 
-        onChange={this.handleLocationChange}
-        id="location"
-        value={this.state.location}
-        placeholder="Location"
-        />
+          Location
+          <input
+            onChange={this.handleLocationChange}
+            id="location"
+            value={this.state.location}
+            placeholder="Location"
+          />
         </label>
-        </div>
-    )
-}
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={this.state.breed}
+            onChange={this.handleBreedChange}
+            onBlur={this.handleBreedChange}
+            disabled={!this.state.breeds.length}
+          >
+          <option/>
+          {this.state.breeds.map(breed=>(
+              <option key={breed} value={breed}>
+              {breed}
+              </option>
+          ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default SearchParams;
